94a88e3b1b42941f284a3c81c36476d978648317
Igor Morais <igor.morais@hollowsoft.com>
2017-02-03 10:16:31 -0200

    Minor refactor

--- Print/app/src/main/java/hollowsoft/print/Print.kt
+++ Print/app/src/main/java/hollowsoft/print/Print.kt
@@ -2,22 +2,54 @@ package hollowsoft.print
 
 import android.graphics.Bitmap
 import android.view.View
+import java.io.File
+import java.io.FileOutputStream
 
 /**
  * @author Igor Morais
  */
 class Print {
 
-    fun print(view: View) : Bitmap {
+    companion object {
 
-        view.isDrawingCacheEnabled = true
+        val QUALITY = 100
 
-        view.buildDrawingCache()
+        private fun print(view: View) : Bitmap {
 
-        val bitmap = Bitmap.createBitmap(view.drawingCache)
+            view.isDrawingCacheEnabled = true
 
-        view.isDrawingCacheEnabled = false
+            view.buildDrawingCache()
 
-        return bitmap
+            val value = Bitmap.createBitmap(view.drawingCache)
+
+            view.isDrawingCacheEnabled = false
+
+            return value
+        }
+
+        fun print(view: View, layout: Boolean = false) : Bitmap {
+
+            if (layout) {
+
+                view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+
+                view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+            }
+
+            return print(view)
+        }
+
+        fun print(name: String, path: String, view: View, layout: Boolean = false) : String {
+
+            val value = print(view, layout)
+
+            val stream = FileOutputStream(File(path + File.separator + name))
+
+            value.compress(Bitmap.CompressFormat.PNG, QUALITY, stream)
+
+            stream.flush()
+
+            return path + File.separator + name
+        }
     }
 }
